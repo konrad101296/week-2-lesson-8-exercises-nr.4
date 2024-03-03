be2205cc1cbbe862735053727bc3bd0f2936be27
konrad101296 <konradslimak@o2.pl>
2024-03-03 11:06:58 +0100

    Add project files.

---
+++ week 2 lesson 8 exercises nr.4/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("podaj cyfre:");
+int cyfra = int.Parse(Console.ReadLine());
+int liczba = 0;
+int linijka = 1;
+int petla = 0;
+for (int i = 0; i < linijka; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j <= i; j++)
+    {
+        liczba++;
+        if (cyfra >= liczba)
+        {
+            Console.Write(liczba);
+            if (liczba < 10 )
+            {
+                Console.Write(" ");
+            }
+
+        }
+        else if (liczba == cyfra + 1)
+        {
+            petla++;
+        }
+    }
+    linijka++;
+    if (petla == 1)
+    {
+        break;
+    }
+}
